e6abbe3d45eb75a8b270dcab94796f1975dec805
nandosabino <sabinofernando05@gmail.com>
2024-09-12 17:26:10 -0300

    Meu Portfólio

---
+++ scroll.js
@@ -0,0 +1,285 @@
+
+window.scroll = ScrollReveal({reset:true})
+
+
+scroll.reveal('.effect-scroll',
+{
+    duration: 1500,
+    distance: '90px',
+    mobile: true
+})
+
+scroll.reveal('.effect-scroll-2',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay: 500,
+        mobile: true
+    })
+
+scroll.reveal('.effect-img-about',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay: 500,
+        origin: 'left',
+        mobile: true
+    })
+
+scroll.reveal('.effect-text-about',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay: 1000,
+        origin: 'right',
+        mobile: true
+    })
+
+scroll.reveal('.effect-btn-about',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay: 1000,
+        origin: 'right',
+        mobile: true
+    })
+
+scroll.reveal('.skills-title',
+    {
+        duration: 1500,
+        distance: '90px',
+        mobile: true
+    })
+
+scroll.reveal('.description',
+    {
+        duration: 1500,
+        distance: '90px',
+        mobile: true
+    })
+
+scroll.reveal('.effect-skills-1',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay: 500,
+        mobile: true
+    })
+
+scroll.reveal('.effect-skills-2',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 1000,
+        mobile: true
+    })
+
+scroll.reveal('.effect-skills-3',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 1500,
+        mobile: true
+    })
+
+scroll.reveal('.effect-skills-4',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        mobile: true
+    })
+
+scroll.reveal('.effect-skills-5',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 1000,
+        mobile: true
+    })
+
+scroll.reveal('.effect-skills-6',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 1500,
+        mobile: true
+    })
+
+scroll.reveal('.effect-skills-7',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        mobile: true
+    })
+
+scroll.reveal('.effect-skills-8',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 1000,
+        mobile: true
+    })
+
+scroll.reveal('.main-text',
+    {
+        duration: 1500,
+        distance: '90px',
+        mobile: true
+    })
+
+scroll.reveal('.skill-left',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay: 1000,
+        origin: 'left',
+        mobile: true
+    })
+
+scroll.reveal('.skill-right',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay: 1000,
+        origin: 'right',
+        mobile: true
+    })
+
+scroll.reveal('.wrapper',
+    {
+        duration: 1500,
+        distance: '90px',
+        mobile: true
+    })
+
+scroll.reveal('.effect-scroll-left',
+    {
+        duration: 1500,
+        distance: '100%',
+        delay: 500,
+        origin: 'left',
+        easing: 'ease-out',
+        mobile: true
+    })
+
+scroll.reveal('.effect-scroll-right',
+    {
+        duration: 1500,
+        distance: '100%',
+        delay: 500,
+        origin: 'right',
+        easing: 'ease-out',
+        mobile: true
+    })
+
+scroll.reveal('.effect-scroll-left-2',
+    {
+        duration: 1500,
+        distance: '100%',
+        delay: 500,
+        origin: 'left',
+        easing: 'ease-out',
+        mobile: true
+    })
+
+scroll.reveal('.effect-scroll-right-2',
+    {
+        duration: 1500,
+        distance: '100%',
+        delay: 500,
+        origin: 'right',
+        easing: 'ease-out',
+        mobile: true
+    })
+
+scroll.reveal('.effect-text-projects-left',
+    {
+        duration: 2000,
+        distance: '100%',
+        delay: 1000,
+        origin: 'left',
+        easing: 'ease-out',
+        mobile: true
+    })
+
+scroll.reveal('.effect-text-projects-left-2',
+    {
+        duration: 2000,
+        distance: '100%',
+        delay: 1000,
+        origin: 'left',
+        easing: 'ease-out',
+        mobile: true
+    })
+
+scroll.reveal('.effect-text-projects-right',
+    {
+        duration: 2000,
+        distance: '100%',
+        delay: 1000,
+        origin: 'right',
+        easing: 'ease-out',
+        mobile: true
+    })
+
+    scroll.reveal('.effect-text-projects-right-2',
+    {
+        duration: 2000,
+        distance: '100%',
+        delay: 1000,
+        origin: 'right',
+        easing: 'ease-out',
+        mobile: true
+    })
+
+scroll.reveal('.effect-technologies-left',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay:1500,
+        mobile: true
+    })
+
+scroll.reveal('.effect-technologies-left-2',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay:1500,
+        mobile: true
+    })
+
+scroll.reveal('.effect-technologies-right',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay:1500,
+        mobile: true
+    })
+
+scroll.reveal('.effect-technologies-right-2',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay:1500,
+        mobile: true
+    })
+
+scroll.reveal('.form',
+    {
+        duration: 1500,
+        distance: '90px',
+        delay: 500,
+        origin: 'bottom',
+        mobile: true
+    })
+
+
+
+
+
+
+
+
+
